22658e3e0a6c367979e8c5b91d7490afb6cf6802
zsoolti1es2 <125669791+zsoolti1es2@users.noreply.github.com>
2023-02-21 18:10:57 +0200

    Update Navigation.js

--- Navigation.js
+++ Navigation.js
@@ -1,67 +1,23 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import HomeScreen from './screens/HomeScreen';
-import RouteDetailScreen from './screens/RouteDetailScreen';
-import AddRouteScreen from './screens/AddRouteScreen';
-import ProfileScreen from './screens/ProfileScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import RouteListScreen from '../screens/RouteListScreen';
+import RouteDetailsScreen from '../screens/RouteDetailsScreen';
 
-const HomeStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="RouteDetail" component={RouteDetailScreen} />
-    </Stack.Navigator>
-  );
-};
-
-const AddRouteStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="AddRoute" component={AddRouteScreen} />
-    </Stack.Navigator>
-  );
-};
-
-const ProfileStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Profile" component={ProfileScreen} />
-    </Stack.Navigator>
-  );
-};
+const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === 'Home') {
-            iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name === 'AddRoute') {
-            iconName = focused ? 'add-circle' : 'add-circle-outline';
-          } else if (route.name === 'Profile') {
-            iconName = focused ? 'person-circle' : 'person-circle-outline';
-          }
-
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: 'tomato',
-        inactiveTintColor: 'gray',
-      }}
-    >
-      <Tab.Screen name="Home" component={HomeStack} />
-      <Tab.Screen name="AddRoute" component={AddRouteStack} />
-      <Tab.Screen name="Profile" component={ProfileStack} />
-    </Tab.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="RouteList" component={RouteListScreen} />
+      <Stack.Screen name="RouteDetails" component={RouteDetailsScreen} />
+    </Stack.Navigator>
   );
 };
 
