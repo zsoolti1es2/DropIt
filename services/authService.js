243b3775cd5da4a05ad96de80e7e530a82637e22
zsoolti1es2 <125669791+zsoolti1es2@users.noreply.github.com>
2023-02-21 18:20:42 +0200

    Update authService.js

--- services/authService.js
+++ services/authService.js
@@ -1,68 +1,44 @@
-import { AsyncStorage } from 'react-native';
-import { API_URL } from '../utils/constants';
-import { postData } from '../utils/apiUtils';
+const API_URL = 'http://localhost:5000/api';
 
-export const login = async (email, password) => {
-  try {
-    const response = await fetch(`${API_URL}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email, password })
-    });
+async function login(username, password) {
+  const response = await fetch(`${API_URL}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  });
 
-    const data = await response.json();
-    if (response.ok) {
-      // login successful
-      return { success: true };
-    } else {
-      // login failed
-      return { success: false, message: data.message };
-    }
-  } catch (error) {
-    // network or server error
-    return { success: false, message: 'Network error' };
+  if (!response.ok) {
+    throw new Error('Failed to login');
   }
-};
 
-export const register = async (email, password) => {
-  try {
-    const response = await fetch(`${API_URL}/register`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email, password })
-    });
+  const data = await response.json();
+  return data;
+}
 
-    const data = await response.json();
-    if (response.ok) {
-      // registration successful
-      return { success: true };
-    } else {
-      // registration failed
-      return { success: false, message: data.message };
-    }
-  } catch (error) {
-    // network or server error
-    return { success: false, message: 'Network error' };
+async function register(username, password, email) {
+  const response = await fetch(`${API_URL}/auth/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username,
+      password,
+      email,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to register');
   }
-};
 
-export const logout = async () => {
-  try {
-    await AsyncStorage.removeItem('token');
-  } catch (error) {
-    throw error;
-  }
-};
+  const data = await response.json();
+  return data;
+}
 
-export const isAuthenticated = async () => {
-  try {
-    const token = await AsyncStorage.getItem('token');
-    return token !== null;
-  } catch (error) {
-    throw error;
-  }
-};
+export { login, register };
