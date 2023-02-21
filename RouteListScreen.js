dbb210ede2d91a5afae0d66379cbebb5b9039555
zsoolti1es2 <125669791+zsoolti1es2@users.noreply.github.com>
2023-02-21 18:13:08 +0200

    Update RouteListScreen.js

--- RouteListScreen.js
+++ RouteListScreen.js
@@ -1,38 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import { useIsFocused } from '@react-navigation/native';
-import { getRoutes } from '../api/routes';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 
 const RouteListScreen = ({ navigation }) => {
-  const isFocused = useIsFocused();
   const [routes, setRoutes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (isFocused) {
-      getRoutes().then(data => {
+    const loadRoutes = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/routes');
+        const data = await response.json();
         setRoutes(data);
-      });
-    }
-  }, [isFocused]);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  const renderItem = ({ item }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>From: {item.pickupPoint}</Text>
-      <Text style={styles.title}>To: {item.destination}</Text>
-      <Text style={styles.title}>Departure: {item.date}</Text>
-      <Text style={styles.title}>Price: {item.price}</Text>
-      <Text style={styles.title}>Parcel size: {item.parcelSize}</Text>
-      <Text style={styles.title}>Contact person: {item.contactPerson}</Text>
-      <Text style={styles.title}>Contact phone: {item.contactPhone}</Text>
-    </View>
+    loadRoutes();
+  }, []);
+
+  const renderRoute = ({ item }) => (
+    <TouchableOpacity
+      style={styles.routeContainer}
+      onPress={() => navigation.navigate('Route Details', { routeId: item._id })}
+    >
+      <Text style={styles.routeName}>{item.name}</Text>
+      <Text style={styles.routeDescription}>{item.description}</Text>
+    </TouchableOpacity>
   );
+
+  if (isLoading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <Text>Loading routes...</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.errorContainer}>
+        <Text>An error occurred: {error}</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
       <FlatList
         data={routes}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
+        keyExtractor={(item) => item._id}
+        renderItem={renderRoute}
       />
     </View>
   );
@@ -41,16 +62,30 @@ const RouteListScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 20,
+    padding: 16,
   },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+  routeContainer: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
-  title: {
+  routeName: {
     fontSize: 18,
+    fontWeight: 'bold',
+  },
+  routeDescription: {
+    fontSize: 14,
+    color: '#666',
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
